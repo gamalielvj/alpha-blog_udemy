1db6a74eade8c2c11888730fa893f71f34482149
Gamaliel Vazquez <gamalielvj@yahoo.com.mx>
2019-04-14 04:09:11 +0000

    add edit update users Seccion 6 clase 121

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users, except:[:new]
+  resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#home'
   get 'about', to: 'pages#about'
-  
-  #get 'pages/home', to: 'pages#home'
-  
-  #get 'pages/about', to: 'pages#about'
-  resources :articles
-
+  get 'signup', to: 'users#new'
 
 end
